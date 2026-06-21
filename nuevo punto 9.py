77b7477eb5de56e9f4ef8bfdc168677766c3ce15
Wanda <wanda.rocio.villegas@gmail.com>
2026-06-20 23:59:55 -0300

    Update nuevo punto 9.py

--- nuevo punto 9.py
+++ nuevo punto 9.py
@@ -9,29 +9,12 @@ audio_rapido = "rapido.wav"
 fs_entrada, x = wavfile.read(audio_lento)
 signal_entrada = x / np.max(np.abs(x))
 
-fs_rapido, x = wavfile.read(audio_rapido)
-signal_rapida = x/ np.max(np.abs(x))
+fs_rapido, y = wavfile.read(audio_rapido)
+signal_rapida =  y / np.max(np.abs(y))
 
 
+################################# filtro antialiasing #################################
 
-#################################   expansor   #################################
-N = 2 #factor expanstor
-
-
-def expansor(x, N):
-    y = np.zeros(N * len(x)) #por defecto es una señal llena de ceros
-
-    y[::N] = x  #intercalo por el factor expansor con valores de la señal x
-
-    return y
-
-signal_expandida = expansor(signal_rapida, N)
-fs_expandida = fs_rapido * N #porque tengo el doble de muestras
-
-#espectralmente se comprime el eje de frecuencia y se generan copias, las elimino con un interpolador 
-
-
-#################################  interpolador  #################################
 def filtro_pasabajos_ventana(fc, fs, orden):
     h = np.zeros(orden + 1) #lleno un array de ceros con el tamaño de cantidad de coeficientes = orden + 1
     wc = 2 * np.pi * fc / fs  #fc a wc discreta
@@ -51,6 +34,11 @@ def filtro_pasabajos_ventana(fc, fs, orden):
 
     return h
 
+"""Tipo de ventanas
+- rectangular       w = 1
+- hann              w = 0.5 - 0.5*np.cos(2*np.pi*n/orden)
+- blackman          w = 0.42 - 0.5*np.cos(2*np.pi*n/orden) + 0.08*np.cos(4*np.pi*n/orden)
+"""
 
 def convolucion(x, h):
     y = np.zeros(len(x)) #lleno un array de ceros con el mismo tamaño que x
@@ -64,56 +52,57 @@ def convolucion(x, h):
 
     return y
 
-
-def interpolador_ideal(x, fs, N):
-    orden = 100
-    fc = fs / (2*N)
-
+def filtro_antialiasing(x, fc, fs, orden):
     h = filtro_pasabajos_ventana(fc, fs, orden)
-    signal_interpolada = convolucion(x, h)
-    signal_interpolada *= N         # ganancia del interpolador
+    y = convolucion(x, h)
 
-    return signal_interpolada
+    return y
+
+N = 2 #factor del decimador
+fc = fs_entrada /(2 * N )  #frecuencia de corte
+orden =  100 #calidad del filtro
 
 
-signal_salida = interpolador_ideal(signal_expandida, fs_rapido, N)
+signal_filtrada = filtro_antialiasing(signal_entrada, fc, fs_entrada, orden)
 
+
+#################################     decimador       #################################
+
+def decimador(x, n):
+    return x[::n]
+
+signal_salida = decimador(signal_filtrada, N)
+fs_salida = fs_entrada
 
 
 #################################   señal de salida   #################################
 
-fs_salida = fs_rapido
-sf.write("audiorapido_velocidad_lenta.wav", signal_salida, fs_salida)
+sf.write("audiolento_velocidadx2.wav", signal_salida, fs_salida) #mismo fs que la original para que sea el doble de la velocidad (lógica)
 
 
 #################################   gráficos temporales  #################################
-
-t_rapido = np.arange(len(signal_rapida)) / fs_rapido
-t_expandida = np.arange(len(signal_expandida)) / fs_rapido
-t_salida = np.arange(len(signal_salida)) /fs_salida
-
 t_entrada = np.arange(len(signal_entrada)) /fs_entrada
+t_salida = np.arange(len(signal_salida)) /fs_salida
+t_rapido = np.arange(len(signal_rapida)) / fs_rapido
 
 plt.figure(figsize=(12, 8))
 plt.subplot(3, 1, 1)
-plt.plot(t_rapido, signal_rapida)
-plt.title("Señal de voz rápida")
+plt.plot(t_entrada, signal_entrada)
+plt.title("Señal de voz lenta")
 plt.ylabel("Amplitud")
 plt.xlabel("Tiempo [s]")
 plt.grid()
-
 
 plt.subplot(3, 1, 2)
-plt.plot(t_expandida, signal_expandida)
-plt.title("Señal de voz rápida expandida")
+plt.plot(t_entrada, signal_filtrada)
+plt.title("Señal de voz lenta filtrada")
 plt.ylabel("Amplitud")
 plt.xlabel("Tiempo [s]")
 plt.grid()
-
 
 plt.subplot(3, 1, 3)
 plt.plot(t_salida, signal_salida)
-plt.title("Señal de voz rápida expandida filtrada")
+plt.title("Señal de voz lenta filtrada decimada")
 plt.ylabel("Amplitud")
 plt.xlabel("Tiempo [s]")
 plt.grid()
@@ -123,15 +112,15 @@ plt.show()
 
 plt.subplot(2, 1, 1)
 plt.plot(t_salida, signal_salida)
-plt.title("Señal de voz rápida expandida filtrada")
+plt.title("Señal de voz lenta filtrada decimada")
 plt.ylabel("Amplitud")
 plt.xlabel("Tiempo [s]")
 plt.grid()
 
 
 plt.subplot(2, 1, 2)
-plt.plot(t_entrada, signal_entrada)
-plt.title("Señal de voz lenta")
+plt.plot(t_rapido, signal_rapida)
+plt.title("Señal de voz rápida")
 plt.ylabel("Amplitud")
 plt.xlabel("Tiempo [s]")
 plt.grid()
@@ -141,7 +130,6 @@ plt.show()
 
 
 #################################   espectrogramas  #################################
-
 def espectrograma_angosta_completo(x, fs, NFFT, nombre):       
     overlap = 0.9     
 
@@ -164,7 +152,7 @@ def espectrograma_ancha_completo(x, fs, NFFT,nombre):
     noverlap = int(NFFT*overlap)  
     figsize = (10, 6)
     plt.figure(figsize=figsize)
-    plt.specgram(x, Fs=fs, NFFT=NFFT, noverlap=noverlap, cmap='RdBu_r', vmin=-110, vmax=-30)  
+    plt.specgram(x, Fs=fs, NFFT=NFFT, noverlap=noverlap, cmap='RdBu_r', vmin=-110, vmax=-30, pad_to=2048)  
     plt.ylim(0, 4000)  
     plt.colorbar(label='Intensidad (dB)')
     plt.title(f'Espectrograma de banda ancha de la señal {nombre}')
@@ -173,24 +161,29 @@ def espectrograma_ancha_completo(x, fs, NFFT,nombre):
     plt.tight_layout()
     plt.show()
 
+#se desplazan los formantes entonces necesita otro ylim
+def espectrograma_ancha_salida(x, fs, NFFT,nombre):
+    overlap = 0.9      
 
+    noverlap = int(NFFT*overlap)  
+    figsize = (10, 6)
+    plt.figure(figsize=figsize)
+    plt.specgram(x, Fs=fs, NFFT=NFFT, noverlap=noverlap, cmap='RdBu_r', vmin=-110, vmax=-30, pad_to=2048)  
+    plt.ylim(0, 6000)  
+    plt.colorbar(label='Intensidad (dB)')
+    plt.title(f'Espectrograma de banda ancha de la señal {nombre}')
+    plt.xlabel('Tiempo (s)')
+    plt.ylabel('Frecuencia (Hz)')
+    plt.tight_layout()
+    plt.show()
+
+
+espectrograma_angosta_completo(signal_entrada, fs_entrada, int(20480), "original")
+espectrograma_angosta_completo(signal_filtrada, fs_entrada, int(20480), "filtrada")
+espectrograma_angosta_completo(signal_salida, fs_salida, int(20480),"salida")
 espectrograma_angosta_completo(signal_rapida, fs_rapido, int(4096), "rápida") 
+
+espectrograma_ancha_completo(signal_entrada, fs_entrada, int(1040), "original")
+espectrograma_ancha_completo(signal_filtrada, fs_entrada, int(1040), "filtrada")
+espectrograma_ancha_salida(signal_salida, fs_salida, int(512),"salida")
 espectrograma_ancha_completo(signal_rapida, fs_rapido, int(512), "rápida")
-
-espectrograma_angosta_completo(signal_expandida, fs_expandida, int(4096), "expandida")
-espectrograma_ancha_completo(signal_expandida, fs_expandida, int(1024), "expandida") 
-
-espectrograma_angosta_completo(signal_salida, fs_salida, int(4096), "salida") 
-espectrograma_ancha_completo(signal_salida, fs_salida, int(512), "salida") 
-
-espectrograma_angosta_completo(signal_entrada, fs_entrada, int(20480), "lenta")
-espectrograma_ancha_completo(signal_entrada, fs_entrada, int(1040), "lenta")
-
-
-
-############################  datos útiles   ############################
-
-print("Rapida:", len(signal_rapida)/fs_rapido)
-print("Expandida:", len(signal_expandida)/fs_rapido)
-print("Salida:", len(signal_salida)/fs_salida)
-print("Lenta:", len(signal_entrada)/fs_entrada)
